2a7417b78b15c338b23d43c9459171d7f3a8b52a
xibjrt <jvaiciulyte@gmail.com>
2020-03-29 21:57:15 +0300

    Updates

---
+++ precache-manifest.66b7ce5db30a9bbd7891b18c978048df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3cb1a59efe75c75b158010ab782e42",
+    "url": "/KABADA-first/index.html"
+  },
+  {
+    "revision": "09bba5d2f3ed5d5421fe",
+    "url": "/KABADA-first/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c65baeea8f2c51e2dcc6",
+    "url": "/KABADA-first/static/js/2.2c60708e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KABADA-first/static/js/2.2c60708e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bba5d2f3ed5d5421fe",
+    "url": "/KABADA-first/static/js/main.c3b9b234.chunk.js"
+  },
+  {
+    "revision": "539579cd8bb8133ad833",
+    "url": "/KABADA-first/static/js/runtime-main.48d64a91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KABADA-first/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/KABADA-first/precache-manifest.66b7ce5db30a9bbd7891b18c978048df.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KABADA-first/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
